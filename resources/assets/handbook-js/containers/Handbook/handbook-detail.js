0302dc71ff54a2159ac8262251d7baaab9778dbe
David Nguyen <david@wthapps.com>
2019-03-08 14:49:48 +0700

    update

--- resources/assets/handbook-js/containers/Handbook/handbook-detail.js
+++ resources/assets/handbook-js/containers/Handbook/handbook-detail.js
@@ -2,9 +2,8 @@ import React, { Component } from "react";
 import "./styles.css";
 import { Calendar } from "primereact/calendar";
 import { Editor } from "primereact/editor";
-import ReactQuill from "react-quill";
-// import Quill from "quill";
-import QuillInstance from "quill-task-list";
+import Quill from "quill";
+import data from "./data.json";
 
 export default class HandbookDetail extends Component {
   constructor(props) {
@@ -12,35 +11,41 @@ export default class HandbookDetail extends Component {
     this.state = { date: new Date() };
     this.modules = {
       toolbar: [
-        [{ header: [1, 2, false] }],
+        // [{ header: [1, 2, false] }],
         ["bold", "italic", "underline", "strike", "blockquote"],
+        [{ size: ["small", false, "large", "huge"] }], // custom dropdown
         [
           { list: "ordered" },
           { list: "bullet" },
+          { list: "check" },
           { indent: "-1" },
           { indent: "+1" }
         ],
+        // [{ size: [1, 2] }],
+        [{ script: "sub" }, { script: "super" }],
         ["link", "image"],
-        ["clean"],
-        ["task-list"]
-      ],
-      "task-list": true
+        ["clean"]
+      ]
     };
   }
 
   componentDidMount() {
-    var quill = new QuillInstance("#editor", {
+    var Size = Quill.import("attributors/style/size");
+    Size.whitelist = ["small", "large", "huge"];
+    Quill.register(Size, true);
+    var quill = new Quill("#editor", {
       theme: "snow",
       modules: this.modules
     });
+    quill.container.firstChild.innerHTML = data.text;
+    quill.on("text-change", function(delta, oldDelta, source) {
+      console.log(quill.container.firstChild.innerHTML);
+    });
   }
 
-  componentWillMount() {
-    // this.props.actions.getProducts();
-    // var quill = new Quill("#editor", {
-    //   theme: "snow"
-    // });
-  }
+  componentWillMount() {}
+
+  handleChange() {}
 
   render() {
     return (
@@ -49,18 +54,18 @@ export default class HandbookDetail extends Component {
           value={this.state.date}
           onChange={e => this.setState({ date: e.value })}
         />
-        {/* <Editor
-          style={{ height: "320px" }}
-          value={this.state.text}
-          onTextChange={e => this.setState({ text: e.htmlValue })}
-          // headerTemplate={header}
-        /> */}
-        {/* <ReactQuill theme="snow" modules={this.modules} /> */}
-        <div id="editor">
-          <p>Hello World!</p>
-          <p>
-            Some initial <strong>bold</strong> text
-          </p>
+        <div id="editor" />
+        <div>
+          <h4>Daily Routine</h4>
+          <h5>Morning:</h5>
+          <p>Make the beds</p>
+          <p>Make the beds</p>
+          <h5>Afternoon:</h5>
+          <p>Working</p>
+          <p>Working</p>
+          <h5>Everning:</h5>
+          <p>Diner</p>
+          <p>Diner</p>
         </div>
       </div>
     );
